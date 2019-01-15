9f1a8fd14ada7a8fdc6fce8b4d770d51849809db
Rohan289 <rbhowmick184@gmail.com>
2019-01-15 11:46:08 +0530

    Add files via upload

---
+++ filterjs.js
@@ -0,0 +1,18 @@
+var app=angular.module('ab',[]);
+app.controller('ccontroller',function($scope)
+{
+$scope.ninjas=[
+  {
+    name : "Rohan",
+    title : "Bhowmick";
+  },
+  {
+    name : "Ahan" , title : "Bhowmick";
+  },
+  {
+    name : "Hiten" , title : "Mukherjee";
+  }
+
+] ;
+}
+);
